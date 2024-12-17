671cc5b9221ba77416e047b23dd73914a2c5cf98
larmen9179 <lo.j.armendariz@gmail.com>
2024-12-17 16:28:57 -0700

    added new character sprite and objects

---
+++ blaster/Assets/Scripts/shipMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipMovement : MonoBehaviour
+{
+
+    public float rotationSpeed = 200.0f;
+    public float thrustSpeed = 5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotation = -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, rotation);
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * thrustSpeed * Time.deltaTime);
+        }
+    }
+}

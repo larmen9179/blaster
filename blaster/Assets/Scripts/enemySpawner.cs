e247d36e4f3f0b7373e994ad61fe5b438dc349f8
larmen9179 <lo.j.armendariz@gmail.com>
2025-02-02 11:54:34 -0700

    started enemy spawn script

--- blaster/Assets/Scripts/enemySpawner.cs
+++ blaster/Assets/Scripts/enemySpawner.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class enemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public float spawnRate = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnEnemy", 0f, spawnRate);
     }
 
     // Update is called once per frame
@@ -16,4 +17,16 @@ public class enemySpawner : MonoBehaviour
     {
         
     }
+
+    void SpawnEnemy()
+    {
+        float randomX = Random.Range(0f, 1f);
+        float randomY = Random.Range(0f, 1f);
+
+        Vector3 spawnPosition = Camera.main.ViewportToWorldPoint(new Vector3(randomX, randomY, 0));
+        spawnPosition.z = 0;
+
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }
+
 }

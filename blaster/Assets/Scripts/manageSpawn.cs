fb388119fc059b15926d523add810e3d2fe43846
larmen9179 <lo.j.armendariz@gmail.com>
2025-03-12 18:43:45 -0600

    adjusted round logic for strong enemies

--- blaster/Assets/Scripts/manageSpawn.cs
+++ blaster/Assets/Scripts/manageSpawn.cs
@@ -15,8 +15,9 @@ public class manageSpawn : MonoBehaviour
     private float planeSpawnDistance = 2f;
     private float tankSpawnDistance = 40f;
     private int enemiesAlive;
-    public float level;
-    public int enemyCount;
+    private int roundNumber = 0;
+    public int baseCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,27 +38,71 @@ public class manageSpawn : MonoBehaviour
     {
         Debug.Log("new round starting");
         yield return new WaitForSeconds(5f); // Small delay before next round
-        //ShowUpgrades(); // Show upgrade selection screen
+                                             //ShowUpgrades(); // Show upgrade selection screen
 
         //yield return new WaitUntil(() => upgradeChosen); // Wait for player to choose an upgrade
-        
+
         //roundNumber++;
         //enemiesToSpawn += 2; // Increase enemy count for difficulty
+        roundNumber++;
         startRound();
     }
     
     private void startRound(){
         enemiesAlive = 0;
 
-        for(int i = 0;i < 4;i++){
+        //add 1 to base count every 4 rounds
+
+        for(int i = 0;i < baseCount + (roundNumber % 4);i++){
             spawn("mouse");
-            enemiesAlive += 3;
         }
 
-        spawn("plane");
-        enemiesAlive++;
-        spawn("tank");
-        enemiesAlive++;
+        //spawns special enemies on round presets
+        //EXCEPT when the round is round 0 -> first round
+        if(roundNumber > 0)
+        {   
+            //tank every 4 rounds and base count update
+            if (roundNumber % 4 == 0)
+            {
+                baseCount++;
+                spawn("tank");
+            }
+
+            //plane every 3 rounds
+            if (roundNumber % 3 == 0)
+            {
+                spawn("plane");
+            }
+
+            //plane and tank every 5 rounds
+            if (roundNumber % 5 == 0)
+            {
+                spawn("plane");
+                spawn("tank");
+
+            }
+
+            //small chance for spawning extra enemy of special type
+            int extraSpecial = Random.Range(1, 6);
+            if (extraSpecial == 1)
+            {
+                int specialChance = Random.Range(1, 3);
+                //plane tree
+                if (specialChance == 1)
+                {
+                    spawn("plane");
+                }
+                else //tank tree
+                {
+                    spawn("tank");
+                }
+            }
+        }
+
+        if (baseCount > 8)
+        {
+            baseCount = 8;
+        }
     }
 
     private void spawn(String enemyType){
@@ -68,14 +113,17 @@ public class manageSpawn : MonoBehaviour
         if(enemyType == "mouse"){
             g = enemyMouse;
             distance = mouseSpawnDistance;
+            enemiesAlive += 3;
         }
         else if(enemyType == "plane"){
             g = enemyPlane;
             distance = planeSpawnDistance;
+            enemiesAlive++;
         }
         else if(enemyType == "tank"){
             g = enemyTank;
             distance = tankSpawnDistance;
+            enemiesAlive++;
         }
 
         int side = Random.Range(0, 4);
